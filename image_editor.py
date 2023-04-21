1a82b5f9575f7e107d5b9534b8291f9202d70645
Alicja M <alicjam98@gmail.com>
2023-04-21 18:30:43 +0200

    Add image resizing

--- image_editor.py
+++ image_editor.py
@@ -2,6 +2,8 @@ from tkinter import filedialog
 from PIL import Image, ImageOps, ImageTk
 
 file_path = ""
+MAX_WIDTH = 900
+MAX_HEIGHT = 600
 
 def add_image(image_container):
     global file_path
@@ -23,12 +25,25 @@ def clear_filters(image_container):
     update_image(image, image_container)
 
 def update_image(image, image_container):
-    # Todo add resizing
-    width = int(image.width / 5)
-    height = int(image.height / 5)
-    image = image.resize((width, height), Image.ANTIALIAS)
+    image = resize_image(image)
 
     image_container.config(width=image.width, height=image.height)
     image = ImageTk.PhotoImage(image)
     image_container.image = image
     image_container.create_image(0, 0, image=image, anchor="nw")
+
+def resize_image(image):
+    width = image.width
+    height = image.height
+    ratio = width/height
+
+    if (width > MAX_WIDTH):
+        width = MAX_WIDTH
+        height = MAX_WIDTH / ratio
+    
+    if (height > MAX_HEIGHT):
+        height = MAX_HEIGHT
+        width = MAX_HEIGHT * ratio
+
+    image = image.resize((int(width), int(height)), Image.ANTIALIAS)
+    return image
